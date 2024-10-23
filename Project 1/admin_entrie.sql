f103d8694c35385729988d14728250b9a2743448
michaelmengistu2 <michaelmengistu.mm@gmail.com>
2024-10-23 12:57:03 -0500

    manually insert admin into Users table

---
+++ Project 1/admin_entrie.sql
@@ -0,0 +1,4 @@
+-- Manually insert admin
+
+INSERT INTO Users (username, password, email, role) 
+VALUES ('admin', '$2b$12$gk8wyyktBwuygqiYbG6OLu9eFGVUVw6lb0aLv8P33RoUk1cfCf3eO', 'admin@example.com', 'admin');
